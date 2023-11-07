d32c0147602c24f3fa5da634e4826e5d6d79035d
OscarSergent <150020702+OscarSergent@users.noreply.github.com>
2023-11-07 11:11:46 +0100

    Add files via upload

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import './Body.css'
+
+function Body () {
+    return (
+        <div className='Body'>
+            <p>corps de page</p>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+const test = 'Hello'
+const test2 = 'World'
+
+    return (
+        <p>{test} {test2}</p>
+    )
+}
+
+export default Footer;
